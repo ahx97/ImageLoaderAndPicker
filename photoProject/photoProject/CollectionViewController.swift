5d2def773ca1efb7f89d63d0dab53746c24a1588
Aaron Henry <ahx97@mail.missouri.edu>
2019-10-18 22:39:42 -0500

    Push

---
+++ photoProject/photoProject/CollectionViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  photoProject
+//
+//  Created by Aaron Henry on 10/18/19.
+//  Copyright © 2019 Aaron Henry. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        
+        
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.allowsEditing = true
+        vc.delegate = self
+        present(vc, animated: true)
+        
+    }
+
+    @objc func addNewPerson(){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        
+        guard let image = info[.editedImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+        
+        print(image.size)
+    }
+
+}
+
